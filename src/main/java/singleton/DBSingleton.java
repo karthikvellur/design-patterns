3238a8dc157fcee6f55d80a12e631886e1dc1580
kvellur <karthik_vellur@intuit.com>
2020-07-08 08:12:18 +0530

    singleton: thread safe

--- src/main/java/singleton/DBSingleton.java
+++ src/main/java/singleton/DBSingleton.java
@@ -2,14 +2,20 @@ package singleton;
 
 public class DBSingleton {
 
-    // Private Constructor
-    // Single instance can only be created
+    // Rules
+    // 1. Private Constructor
+    // 2. Single instance can only be created
 
     // Reference: https://www.geeksforgeeks.org/volatile-keyword-in-java/
     // For Making it thread safe
+    // Thread Safe: Method or class can be used by multiple threads at the same time without any problem
     private static volatile DBSingleton instance = null;
 
     private DBSingleton() {
+        if(instance != null){
+            // Blocks implementations of using reflection to create new object
+            throw  new RuntimeException("Use getInstance() method to create");
+        }
     }
 
     public static DBSingleton getInstance(){
@@ -18,7 +24,15 @@ public class DBSingleton {
         // Helps in bringing up the application faster
         // Reference: https://en.wikipedia.org/wiki/Lazy_loading
         if(instance == null){
-            instance = new DBSingleton();
+            // For making it thread safe we need to used synchronized
+            // Also used synchronized only on block of code rather than whole method
+            // If two threads access at the same time - only one can execute
+            // Additional null check is present so that thread actually creating the object ensures that object is not created already
+            synchronized (DBSingleton.class) {
+                if(instance == null){
+                    instance = new DBSingleton();
+                }
+            }
         }
         return instance;
     }
